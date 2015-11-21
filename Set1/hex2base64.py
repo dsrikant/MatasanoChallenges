6e122c1cf9e083319c2fb9f3f854920dee1404ab
Dushyant Srikant <dushyant.srikant@gmail.com>
2015-11-21 16:44:19 -0600

    Solved base case for hex to base64 conversion

--- Set1/hex2base64.py
+++ Set1/hex2base64.py
@@ -1,15 +1,27 @@
-# 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
-# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+from bitstring import BitArray
 
-# hex to base64
+b64e = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',\
+11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',\
+22:'W',23:'X',24:'Y',25:'Z',26:'a',27:'b',28:'c',29:'d',30:'e',31:'f',32:'g',\
+33:'h',34:'i',35:'j',36:'k',37:'l',38:'m',39:'n',40:'o',41:'p',42:'q',43:'r',\
+44:'s',45:'t',46:'u',47:'v',48:'w',49:'x',50:'y',51:'z',52:'0',53:'1',54:'2',\
+55:'3',56:'4',57:'5',58:'6',59:'7',60:'8',61:'9',62:'+',63:'/'}
 
 
 inp =  '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 out = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
+x = bin(int(inp, base=16)).lstrip('0b')
 
-print len(inp)
-print len(out)
+values = len(x)/6
 
+init = 0
+chomp = 5
+base64 = ''
 
-f = '4927'
+# What if the number of bits is not divisible by 6?
+
+while chomp <= len(x) :
+  base64 = base64 + b64e.get(int(x[init:chomp], 2))
+  init = chomp
+  chomp += 6
